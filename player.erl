003d77439e1c2b8bcde3f7668a4e7eae1ed145ad
unknown <sdd7427@1550-5.se.rit.edu>
2013-04-29 11:47:14 -0400

    got player started and working

--- player.erl
+++ player.erl
@@ -1,16 +1,16 @@
 -module(player).
--export([start/1, status/0]).
+-export([start/1, play/2, status/0]).
 
 start(Name) -> 
-	player = spawn(player, play, {self(), Name}),
-	register(player, player),
-	io:format("started the thread: ~w~n", [player]).
+	Player = spawn(player, play, [self(), Name]),
+	register(player, Player),
+	io:format("started the thread: ~p", [Player]).
 
 play(Pid, Name) -> play(Pid, Name, 0).
 play(Pid, Name, Start_room) ->
 	receive 
 		{ClientPid, move} ->
-			io:format("~w~n is moving...", [ClientPid])
+			io:format("~p is moving...", [ClientPid])
 	end,
 	play(Pid, Name, Start_room).
 
