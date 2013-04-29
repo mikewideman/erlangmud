39c48006f4e881b830604552deb0af9a3fbbfc54
Shaun DeVos <mk7upurz87@gmail.com>
2013-04-29 13:10:11 -0400

    code formatting

--- player.erl
+++ player.erl
@@ -8,29 +8,30 @@ start(Name) ->
 
 play(Pid, Name) -> play(Pid, Name, 0).
 
-play(Pid, Name, Start_room) ->
+play(Pid, Name, Curr_room) ->
 	receive 
-		{ClientPid, move} ->
-			io:format("~p is moving...", [ClientPid]);
+		{Pid, move} ->
+			io:format("~p is moving...", [Pid]);
 			
-        {Start_room, player_command, {Action}} ->
+        {Curr_room, player_command, {Action}} ->
 		
             % would Action have already been a completely parsed command which
             % we know to be valid, or do we validate whether we can perform
             % the command the user sent? e.g. if user says drop sword,
             % but we don't have a sword, who figures this out, the Room or the
             % Player?
-            {Results} = room:receiveAction(self(), Start_room, {Action},
-            case {Results} of
+            {Results} = room:receiveAction(self(), Curr_room, {Action},
+            case Results of
                 {died} ->
-                    NewPlayerRecord = {Pid, "I'm dead", Start_room};
+                    NewPlayerRecord = {Pid, "I'm dead", Curr_room};
                 {entered, NewRoom} ->
                     NewPlayerRecord = {Pid, Name, NewRoom};
                 _ ->
                     ok
+            end
 	end,
 	
     % have play take the player record as its argument
-	play(Pid, Name, Start_room).
+	play(Pid, Name, Curr_room).
 
 status() -> self().
