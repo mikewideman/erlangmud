e2e8e054e512c947a6aa24452328f1b7efb3728b
Andrew Garrett <ajg9132@rit.edu>
2013-04-30 11:57:52 -0400

    created character.hrl
    should contain common records and functions for both PCs and NPCs

---
+++ character.hrl
@@ -0,0 +1,38 @@
+%%%=============================================================================
+%%% @doc Character interface for PCs and NPCs
+%%% Characters share many characteristics (ha!) in common.
+%%% @end
+%%%=============================================================================
+
+%% @doc A character's state.
+%% `id': the unique identifier for the character.
+%%
+%% `name': a human-readable string which will be displayed to the user as the
+%% name of the character and will be used to refer to the character in commands,
+%% but need not be unique.
+%%
+%% `health': a number which must be at least 0. A health of 0 indicates that
+%% the character is dead, otherwise the character is alive.
+%%
+%% `inventory': a list of items which the character is carrying.
+%%
+%% `room': the pid of the room in which the character resides. Character
+%% processes must speak to the room to perform actions.
+%% @end
+-record(character,
+    { id :: reference()
+    , name :: string()
+    , health :: non_neg_integer()
+    , inventory = [] :: list()
+    , room :: pid()
+    }).
+
+-spec make_character(string(), non_neg_integer(), pid()) -> #character{}.
+%% @doc Returns a new character.
+make_character(Name, Health, Room) ->
+    #character
+        { id = make_ref()
+        , name = Name
+        , health = Health
+        , room = Room
+        }.
