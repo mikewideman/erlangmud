a1c3691ba705788fc5cd993aa1259763659dcf9b
Andrew Garrett <ajg9132@rit.edu>
2013-04-30 21:05:58 -0400

    action and event hrls for type defs

---
+++ action.hrl
@@ -0,0 +1,13 @@
+%%%=============================================================================
+%%% @doc Definitions of game actions.
+%%% This header defines the types of actions which can be performed as well as
+%%% how they are represented as data structures.
+%%%
+%%% Actions are an expression of things which characters do in the game, e.g.
+%%% attack.
+%%%
+%%% These actions will be formed by character modules and passed to rooms, where
+%%% they will be interpreted and their results carried out.
+%%% @end
+%%%=============================================================================
+
---
+++ event.hrl
@@ -0,0 +1,16 @@
+%%%=============================================================================
+%%% @doc Definitions of game events.
+%%% This header defines the types of events which can occur as well as how they
+%%% are represented as data structures.
+%%%
+%%% Events are an expression of things which happen in the game as a result of
+%%% another event or an action - but events are not actions. For example,
+%%% "attack" is an action, but being attacked is not - it is an event.
+%%% Similarly, the occurrence of an attack being performed is an event, i.e.
+%%% "attacking".
+%%%
+%%% These events will be formed passively by characters and items as well as
+%%% actively by rooms so their meaning can be interpreted by the room in which
+%%% they occur and the concerned parties can be notified of what occurred.
+%%% @end
+%%%=============================================================================
