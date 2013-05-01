9b212c31e1de3403330f1eaecad9d1aeef87448d
Andrew Garrett <ajg9132@rit.edu>
2013-04-30 21:33:00 -0400

    define verb and action types

--- action.hrl
+++ action.hrl
@@ -11,3 +11,14 @@
 %%% @end
 %%%=============================================================================
 
+-type verb() ::
+      'attack'
+    | 'enter'
+    %% @todo define more verbs
+    .
+%% verb() is an atom which is recognized as a valid verb in a command sentence
+%% issued by the user. In general, verbs are the valid "kinds" of actions.
+
+-type action() :: {verb(), Subject :: pid(), Object :: pid()}.
+%% action() is a sentence, which contains a verb, a subject, and an object.
+%% @todo consider defining a type for subjects and objects.
