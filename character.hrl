5b79fdda1118425600b4ee615470b19f043081ab
Andrew Garrett <ajg9132@rit.edu>
2013-05-01 12:43:56 -0400

    considering a type or record for static attrs
    character's pids, ids, and names should not change, so it will be
    helpful to associate them together so that rooms can hold onto this data
    without having to query for it every time someone refers to it.

--- character.hrl
+++ character.hrl
@@ -40,3 +40,9 @@ make_character(Name, Health, Attack, Room) ->
         , attack = Attack
         , room = Room
         }.
+
+% %% @todo consider whether we want a record or a type for this
+% -type character() :: {Proc :: pid(), Id :: reference(), Name :: string()}.
+% %% character() is a type which includes a character process and all of its
+% %% record data which will not change, which is useful for avoiding unnecessary
+% %% queries to other processes
