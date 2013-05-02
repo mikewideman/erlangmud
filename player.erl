30d99ae437af908dd78c0be793fd088bbb469b61
Andrew Garrett <ajg9132@rit.edu>
2013-05-01 22:00:36 -0400

    commenting out old code, todo notes

--- player.erl
+++ player.erl
@@ -8,41 +8,41 @@
 -export([start/1, play/2, status/0]).
 -include("character.hrl").
 
-start(Name) -> 
-	Player = spawn(player, play, [self(), Name]),
-	register(player, Player),
-	io:format("started the thread: ~p", [Player]).
+% start(Name) -> 
+	% Player = spawn(player, play, [self(), Name]),
+	% register(player, Player),
+	% io:format("started the thread: ~p", [Player]).
 
-play(Pid, Name) -> play(Pid, Name, 0).
+% play(Pid, Name) -> play(Pid, Name, 0).
 
-play(Pid, Name, Curr_room) ->
-	receive 
-		{Pid, move} ->
-			io:format("~p is moving...", [Pid])
+% play(Pid, Name, Curr_room) ->
+	% receive 
+		% {Pid, move} ->
+			% io:format("~p is moving...", [Pid])
 			
-        %{Start_room, player_command, {Action}} ->
+        % %{Start_room, player_command, {Action}} ->
 		
-            % would Action have already been a completely parsed command which
-            % we know to be valid, or do we validate whether we can perform
-            % the command the user sent? e.g. if user says drop sword,
-            % but we don't have a sword, who figures this out, the Room or the
-            % Player?
-        %    {Results} = room:receiveAction(self(), Start_room, {Action},
-        %    
-        %    NewPlayerRecord = case {Results} of
-        %        {died} ->
-        %            {Pid, "I'm dead", Start_room};
-        %        {entered, NewRoom} ->
-        %            {Pid, Name, NewRoom};
-        %        _ ->
-        %            {Pid, Name, Start_room}
-        %    end
-	end,
+            % % would Action have already been a completely parsed command which
+            % % we know to be valid, or do we validate whether we can perform
+            % % the command the user sent? e.g. if user says drop sword,
+            % % but we don't have a sword, who figures this out, the Room or the
+            % % Player?
+        % %    {Results} = room:receiveAction(self(), Start_room, {Action},
+        % %    
+        % %    NewPlayerRecord = case {Results} of
+        % %        {died} ->
+        % %            {Pid, "I'm dead", Start_room};
+        % %        {entered, NewRoom} ->
+        % %            {Pid, Name, NewRoom};
+        % %        _ ->
+        % %            {Pid, Name, Start_room}
+        % %    end
+	% end,
 	
-    % have play take the player record as its argument
-	play(Pid, Name, Curr_room).
+    % % have play take the player record as its argument
+	% play(Pid, Name, Curr_room).
 
-status() -> self().
+% status() -> self().
 
 
 %%% Just a different approach here, don't worry
@@ -60,7 +60,10 @@ start(Name, Health, Attack, Room) ->
 %% @end
 main(Player) ->
     NewPlayer = receive
-        _ -> Player
+    %% @todo respond to events
+        _ ->
+            %% @todo send actions
+            Player
     after 0 ->
         Player
     end,
