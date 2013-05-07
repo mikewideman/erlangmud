8517e069a7d58cf14c8d6b491b298ef428d742da
Mike Wideman <wideman.mb@gmail.com>
2013-05-07 10:17:46 -0400

    Adding parser

---
+++ parser.erl
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Command Parser
+% Takes human input (i.e. "Attack troll with sword")
+% and parses it into a game-usable format.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-module(parser).
+-export( [ parse/1 ] ).
+
+parse(String) ->
+	String.
