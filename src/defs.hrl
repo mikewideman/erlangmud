787276cbb3e4c0370535b0574326f7be5836a5e6
Julian Boilen <julianboilen@gmail.com>
2013-05-11 17:48:45 -0400

    Added a master error type. Please append to it.

--- src/defs.hrl
+++ src/defs.hrl
@@ -11,16 +11,28 @@
 -type verb() :: atom().
 %% verb() is an atom which is recognized as a valid verb in a command sentence
 %% issued by the user or by character actions in general. In other words, verbs
-%% are the valid "kinds" of actions, although it is possible to union other atoms
-%% with verb() in function specs to make other "kinds" of actions admissible in
-%% certain contexts.
-
 -type payload_value() ::
       {'damage', Damage :: non_neg_integer()}   %% DamageTaken or DamageDone
     | {atom(), any()}
     .
+
+-type thing() :: #character_proc{}.
 %% payload_value() is a special kind of tuple which is made of an atomic
 %% tag which describes the data and the data itself.
+
+%all the errors! (should be defined here please. A master list of errors keeps me ffrom having to search every file again to find what could go wrong)
+-type error() :: 
+	{'error, 
+		%in room, when trying to target an action with something not in the room
+		{'notInRoom', Thing :: thing()} |
+
+		%used for targeting Input, when strings cannot be matched to room contents
+		{'multipleMatches' | 'notInRoom', 'directObject' | 'indirectObject'} |
+
+		%used for things when they do not support an action that occured on them
+		%not actually implemented anywhere right now, but should be
+		'doesNotUnderstand'
+	}.
 
 %%%%%%%%%%%%%%%
 %%% Records %%%
