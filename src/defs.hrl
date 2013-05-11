e5ad901f37f6a50454e8edd80f27e86b65b67b32
Julian Boilen <julianboilen@gmail.com>
2013-05-11 13:53:14 -0400

    corrected input record
    subject type should be character_proc since dungeon knows the player when it creates the input

--- src/defs.hrl
+++ src/defs.hrl
@@ -43,7 +43,7 @@
 
 -record(input,
     { verb          :: verb()
-    , subject       :: string()
+    , subject       :: #character_proc{}
     , object        :: string()
     , payload = []  :: list(payload_value())
     }).
