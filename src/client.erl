9a15585ee650bab3650bbf2146b294bee304a0a0
Sam Lucidi <mansam@csh.rit.edu>
2013-05-15 23:26:52 -0400

    fixed client

--- src/client.erl
+++ src/client.erl
@@ -16,11 +16,8 @@ outputloop() ->
 	receive 
 	{fail, GameAction} ->
 		io:format(" Your action ~s failed.", [GameAction#action.verb] );
-	{event, Event} when Event#event.verb == look ->
-		io:format(Event#event.payload),
-		io:format("\n");
 	{event, Event} ->
-		io:format("New event: \n Verb:~p\nSubject:~p\nObject:~p\n\nPayload:~p\n ",
+		io:format("New event: ~n Verb:~p~nSubject:~p~nObject:~p~nPayload:~p~n",
 		  [Event#event.verb, Event#event.subject, Event#event.object, Event#event.payload])
 	end.
 
